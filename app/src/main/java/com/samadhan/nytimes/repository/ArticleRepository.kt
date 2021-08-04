803288947ec602d26db6b40ddb9606c4d8973f8f
P115189 <winjit.samadhan.medge@fullertonindia.com>
2021-08-04 23:31:21 +0530

    Optimise ArticleRepository.kt class

--- app/src/main/java/com/samadhan/nytimes/repository/ArticleRepository.kt
+++ app/src/main/java/com/samadhan/nytimes/repository/ArticleRepository.kt
@@ -1,42 +1,29 @@
 package com.samadhan.nytimes.repository
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import com.samadhan.nytimes.retrofit.RetrofitClient
 import com.samadhan.nytimes.BuildConfig
 import com.samadhan.nytimes.model.EntityArticleOutputs
-import com.samadhan.nytimes.utils.ObjectConvertor
+import com.samadhan.nytimes.retrofit.RetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 object ArticleRepository {
-
     val serviceSetterGetter = MutableLiveData<EntityArticleOutputs>()
-
     fun getServicesApiCall(): MutableLiveData<EntityArticleOutputs> {
-
         val call = RetrofitClient.apiInterface.getMostPopularArticles(BuildConfig.APP_KEY)
-
-        call.enqueue(object: Callback<EntityArticleOutputs> {
+        call.enqueue(object : Callback<EntityArticleOutputs> {
             override fun onFailure(call: Call<EntityArticleOutputs>, t: Throwable) {
-                Log.v("DEBUG : ", t.message.toString())
             }
 
             override fun onResponse(
                 call: Call<EntityArticleOutputs>,
                 response: Response<EntityArticleOutputs>
             ) {
-                Log.v("DEBUG : ", response.body().toString())
-
-                val data = response.body()
-
-                val msg = "Hi"//data!!.message
-
-                serviceSetterGetter.value = response.body()//ObjectConvertor.convertStringToObject(response.body().toString(), EntityArticleOutputs::class.java)
+                serviceSetterGetter.value =
+                    response.body()//ObjectConvertor.convertStringToObject(response.body().toString(), EntityArticleOutputs::class.java)
             }
         })
-
         return serviceSetterGetter
     }
 }
